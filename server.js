d798d665aa484227f84c1fcec3d40ed1d7cb94b0
Logesh T <53284256+Logesh18@users.noreply.github.com>
2021-12-06 19:41:36 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8081;
+
+//app.use('/static', express.static('public'))
+app.use(express.static('public'))
+app.listen(port, () => console.log(`listening on port ${port}!`));
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
